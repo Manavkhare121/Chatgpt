29d8ab3f75a265c1ceeef3b24656d2a75df99a64
Manav Khare <manavkhare121@gmail.com>
2026-02-07 23:59:03 +0530

    integrate Google GenAI for AI response generation and implement socket authentication

--- Backend/src/sockets/socket.server.js
+++ Backend/src/sockets/socket.server.js
@@ -1,10 +1,44 @@
-import { Server, Socket } from "socket.io";
+import { Server } from "socket.io";
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
+import { usermodel } from "../models/user.model.js";
+import { generateResponse } from "../services/ai.service.js";
 
-function initSocketServer(httpServer){
-    const io=new Server(httpServer,{})
-    io.on("connection",(Socket)=>{
-        console.log("New Socket Connection:",Socket.id)
-    })
+function initSocketServer(httpServer) {
+    const io = new Server(httpServer, {});
+
+    // Socket.io auth middleware
+    io.use(async (socket, next) => {
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+        if (!cookies.token) {
+            return next(new Error("Authentication error: No token Provided"));
+        }
+
+        try {
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+            const user = await usermodel.findById(decoded.id);
+            socket.user = user;
+            next();
+        } catch (error) {
+            next(new Error("Authentication error: Invalid token"));
+        }
+    });
+
+    io.on("connection", (socket) => {
+        console.log("New socket connection:", socket.id);
+
+        socket.on("ai-message", async (messagePayload) => {
+            console.log(messagePayload);
+
+            const response = await generateResponse(messagePayload.message);
+
+            socket.emit("ai-response", {
+                content: response,
+                chat: messagePayload.chat,
+            });
+        });
+    });
 }
 
 export default initSocketServer;
