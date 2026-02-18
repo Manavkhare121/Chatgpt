446c234ca2b6330ac128a74da12498504b9b728c
Manav Khare <manavkhare121@gmail.com>
2026-02-19 04:01:18 +0530

    implemented the frontend folder with the AppRoutes,Login ,Register Page initialisation

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,13 @@ import cookieParser from "cookie-parser";
 import express from "express"
 import authroutes from "./routes/auth.routes.js"
 import chatroutes from './routes/chat.routes.js'
+import cors from "cors"
 const app=express();
 
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 app.use(express.json());
 app.use(cookieParser());
  
