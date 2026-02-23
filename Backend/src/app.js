2f1f459175bd37a547de661bec4ba6368e5dbd43
Manav Khare <manavkhare121@gmail.com>
2026-02-23 23:25:09 +0530

    Add initial HTML structure, CSS styles, and SVG icon for the application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,18 +1,36 @@
 import cookieParser from "cookie-parser";
-import express from "express"
-import authroutes from "./routes/auth.routes.js"
-import chatroutes from './routes/chat.routes.js'
-import cors from "cors"
-const app=express();
+import express from "express";
+import authroutes from "./routes/auth.routes.js";
+import chatroutes from "./routes/chat.routes.js";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-app.use(cors({
-    origin:['http://localhost:5173','https://chatgpt-1-4oi8.onrender.com'],
-    credentials:true
-}))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://chatgpt-1-4oi8.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
- 
-app.use('/api/auth',authroutes)
-app.use('/api/chat',chatroutes)
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/api/auth", authroutes);
+app.use("/api/chat", chatroutes);
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 export default app;
